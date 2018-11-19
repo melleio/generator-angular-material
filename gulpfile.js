7cca18ff85ea64b0a4abfbd73fe7f9ae423ccc98
lloyd <lloyd@emelle.me>
2018-11-19 10:59:57 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,204 @@
-'use strict';
-var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var excludeGitignore = require('gulp-exclude-gitignore');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
-var plumber = require('gulp-plumber');
-var coveralls = require('gulp-coveralls');
+var gutil = require('gulp-util');
+var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var inject = require('gulp-inject');
+var imagemin = require('gulp-imagemin');
+var iife = require("gulp-iife");
+var cleanCSS = require('gulp-clean-css');
+var Server = require('karma').Server;
+var historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
+gulp.task('default', ['serve']);
 
-gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
-});
+gulp.task('init', ['sass', 'bower', 'js', 'uglify-js', 'image', 'image-min', 'html', 'index']);
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+// Static Server + watching js/scss/html files
+gulp.task('serve', ['init'], function () {
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
+    browserSync.init({
+        server: {
+            baseDir: './dev',
+            middleware: [historyApiFallback()]
+        }
     });
+
+    /* If you use a proxy replace the previous code with the below script replacing 'yourlocal.dev' with your local proxy
+       
+        browserSync.init({
+            proxy: 'yourlocal.dev'
+        });
+
+   */
+
+    gulp.watch('./scss/*.scss', ['sass-watch']);
+
+    gulp.watch('./public/images/*', ['image-watch']);
+
+    gulp.watch('./public/**/*.html', ['html-watch']);
+
+    gulp.watch('./public/js/**/*.js', ['js-watch']);
+
+    gulp.watch('./bower_components/**/*.js', ['bower']);
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
+
+gulp.task('index', function () {
+    var target = gulp.src('./dev/index.html');
+    var sourcesIndex = gulp.src([
+        './bower_components/**/!(angular.min)*.js',
+        './public/js/config/app.js', './public/js/factories/**/*.js', './public/js/services/**/*.js', './public/js/controllers/**/*.js', './public/js/filters/**/*.js', './public/js/directives/**/*.js', './bower_components/**/*.css', './public/css/**/*.css'], { read: false });
+
+
+    return target
+        //.pipe(inject(sources))
+        .pipe(inject(gulp.src('./bower_components/angular/angular.min.js', { read: false }), { name: 'angular.min.js' }))
+        .pipe(inject(sourcesIndex, { name: 'rest-modules' }))
+        .pipe(gulp.dest('./dev'))
 });
 
-gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
-
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+gulp.task('html', function () {
+    return gulp.src('./public/**/*.html')
+        .pipe(gulp.dest('./dev'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test', 'coveralls']);
+gulp.task('image', function () {
+    return gulp.src('./public/images/*')
+        .pipe(gulp.dest('./dev/public/images'))
+        .pipe(gulp.dest('./dist/public/images'))
+        .pipe(browserSync.stream());
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(gulp.dest('./dev/public/css'))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist/public/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function () {
+    return gulp.src('./public/js/**/*.js')
+        .pipe(gulp.dest('./dev/public/js'))
+});
+
+gulp.task('bower', ['index', 'index:dist'], function () {
+    return gulp.src(['./bower_components/**/*.min.js', './bower_components/**/*.min.css'])
+        .pipe(gulp.dest('./dev/bower_components'))
+        .pipe(gulp.dest('./dist/bower_components'));
+});
+
+gulp.task('image-watch', ['image', 'image-min'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('html-watch', ['html'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('sass-watch', ['sass', 'index', 'index:dist'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('js-watch', ['js', 'uglify-js', 'index', 'index:dist'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+//DIST:
+
+gulp.task('image-min', function () {
+    gulp.src(['./public/**/*.png', './public/**/*.jpg', './public/**/*.gif', './public/**/*.jpeg'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/public'));
+});
+
+gulp.task('uglify-js', function () {
+    return gulp.src(['./public/js/config/app.js', './public/js/factories/**/*.js', './public/js/services/**/*.js', './public/js/controllers/**/*.js', './public/js/filters/**/*.js', './public/js/directives/**/*.js'])
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('./public/js/min/'))
+        // .pipe(uglify({ mangle: false }))
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(gulp.dest('./public/js/min/'));
+});
+
+gulp.task('index:dist', function () {
+    var target = gulp.src('./dist/index.html');
+    var sourcesDist = gulp.src(['./bower_components/**/!(angular.min)*.js', './public/js/min/all.min.js', './bower_components/**/*.css', './public/css/**/*.css'], { read: false });
+
+    return target
+        .pipe(inject(gulp.src('./bower_components/angular/angular.min.js', { read: false }), { name: 'angular.min.js' }))
+        .pipe(inject(sourcesDist, { name: 'rest-modules' }))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('dist:iife', function () {
+    return gulp.src('./public/js/min/all.min.js')
+        .pipe(iife())
+        .pipe(rename('./js/min/all.min.js'))
+        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./dist/public'));
+});
+
+gulp.task('serve:dist', ['dist:package'], function () {
+
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            middleware: [historyApiFallback()]
+        }
+    });
+
+    /* If you use a proxy replace the previous code with the below script replacing 'yourlocal.dev' with your local proxy
+       
+        browserSync.init({
+            proxy: 'yourlocal.dev'
+        });
+
+   */
+
+    gulp.watch('./scss/*.scss', ['sass-watch']);
+
+    gulp.watch('./public/images/*', ['image-watch']);
+
+    gulp.watch('./public/**/*.html', ['html-watch']);
+
+    gulp.watch('./public/js/**/*.js', ['js-watch']);
+
+    gulp.watch('./bower_components/**/*.js', ['bower']);
+});
+
+gulp.task('dist:package', ['sass', 'bower', 'uglify-js', 'image', 'image-min', 'html', 'dist:iife', 'index:dist']);
+
+
+//TDD
+
+/* Run test once and exit */
+
+gulp.task('spec', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+/* Watch for file changes and re-run tests on each change */
+
+gulp.task('serve:spec', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+});
